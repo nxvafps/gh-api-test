61459b435052702801c7b1d39cc1855aa710a65e
nxvafps <oliverquirk11@gmail.com>
2025-02-27 15:36:28 +0000

    implement cron jobs

--- index.ts
+++ index.ts
@@ -1,10 +1,30 @@
 import "dotenv/config";
 import { calculateChangesPerUser, UserContributions } from "./github-api";
+import cron from "node-cron";
 
-calculateChangesPerUser("nxvafps", "pr-test-repo")
-  .then((contributions: UserContributions) => {
-    console.log("Contributions per user:", contributions);
-  })
-  .catch((error: Error) => {
-    console.error("Error fetching contributions:", error);
-  });
+cron.schedule("* * * * *", async () => {
+  try {
+    const contributions: UserContributions = await calculateChangesPerUser(
+      "nxvafps",
+      "pr-test-repo"
+    );
+    console.log(
+      `[${new Date().toISOString()}] Contributions per user:`,
+      contributions
+    );
+  } catch (error) {
+    console.log(
+      `[${new Date().toISOString()}] Error fetching contributions`,
+      error
+    );
+  }
+});
+
+console.log(`Cron job started.`);
+// calculateChangesPerUser("nxvafps", "pr-test-repo")
+//   .then((contributions: UserContributions) => {
+//     console.log("Contributions per user:", contributions);
+//   })
+//   .catch((error: Error) => {
+//     console.error("Error fetching contributions:", error);
+//   });
